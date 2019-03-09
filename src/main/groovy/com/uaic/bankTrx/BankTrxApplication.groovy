302a388390306d0c87d05269b474fce158da9262
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2019-03-09 14:36:12 +0200

    First Commit

---
+++ src/main/groovy/com/uaic/bankTrx/BankTrxApplication.groovy
@@ -0,0 +1,13 @@
+package com.uaic.bankTrx
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BankTrxApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BankTrxApplication, args)
+	}
+
+}
